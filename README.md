a221337179eccf6c0520a716810be973ed0c861b
Darell Tan <darell.tan@gmail.com>
2019-07-24 16:24:34 +0000

    Revamped build script with inspiration from PKGBUILD.
    The script now downloads, unpacks and compiles collectd. All you need to
    do is to set up and specify the correct toolchain to use.

--- README.md
+++ README.md
@@ -6,7 +6,11 @@ This repo helps with cross-compiling collectd for ASUSWRT routers.
 
 1. You need to setup the [ASUSWRT toolchains](https://github.com/RMerl/am-toolchains)
 
-2. Download & unpack [collectd](https://collectd.org/) source (or clone from GitHub)
+2. Use this `build.sh` script to download, build and package collectd:
 
-3. Use this `build.sh` script to build and package collectd
+   ```
+   CROSS_COMPILE=arm-brcm-linux-uclibcgnueabi ./build.sh
+   ```
+
+   You can specify the toolchain to use with the `CROSS_COMPILE` environment variable.
 
--- build.sh
+++ build.sh
@@ -7,11 +7,10 @@
 set -e 
 
 PKGVER=5.9.0
+PKGURL="https://collectd.org/files/collectd-$PKGVER.tar.bz2"
+SHA256SUM=7b220f8898a061f6e7f29a8c16697d1a198277f813da69474a67911097c0626b
 
 OUT_TARBALL=collectd-$PKGVER.tar.gz
-SRCDIR="`dirname \"$0\"`/files"
-
-[ -x ./configure ] || exit
 
 fakeroot -v >/dev/null 2>/dev/null || exit
 
@@ -22,31 +21,87 @@ fi
 
 $CROSS_COMPILE-gcc -v 2>/dev/null || exit
 
-[ -e config.status ] || ./configure --host=$CROSS_COMPILE \
-	--prefix=/opt \
-	--runstatedir=/var/run \
-	--with-fp-layout=nothing --disable-pcie_errors
+checksum() {
+	local filename=$1
+	[ -s "$filename" ] || return 1
+	[ "`sha256sum -b \"$filename\" | cut -d' ' -f1`" = $SHA256SUM ] || return 1
+	return 0
+}
 
-rm -rf installroot
-mkdir  installroot
-make install DESTDIR=$PWD/installroot
+extract() {
+	local fname="`basename "$PKGURL"`"
 
-# FIXME get rid of the .la files
-find installroot -iname '*.la' -exec rm {} \;
+	# download if file is missing or corrupt
+	if ! checksum "$fname"; then
+		curl -L -o "$fname" "$PKGURL"
+		if ! checksum "$fname"; then
+			echo "download failed"
+			exit 1
+		fi
+	fi
 
-# get rid of other files
-find installroot -type f -path '*/perl/*'      -exec rm -f {} \;
-find installroot -type f -path '*/pkgconfig/*' -exec rm -f {} \;
-find installroot -not -type d -iname 'libcollectdclient*' -exec rm -f {} \;
-find installroot -type f -path '*/bin/collectd*' -exec rm -f {} \;
-find installroot -type f -path '*/sbin/collectdmon' -exec rm -f {} \;
-find installroot -depth -empty -exec rmdir {} \;
+	# extract file
+	rm -rf collectd-$PKGVER
+	tar xvf "$fname"
+}
 
-# add in our files
-install -m0644 -D $SRCDIR/collectd.control installroot/opt/lib/ipkg/info/collectd.control
-install -m0755 -D $SRCDIR/S99collectd      installroot/opt/etc/init.d/S99collectd
+prepare() {
+	cd collectd-$PKGVER
+}
 
-# package files
-rm -f "$OUT_TARBALL"
-fakeroot -- tar czvf "$OUT_TARBALL" -C installroot/opt sbin lib etc share/collectd
+build() {
+	cd "${srcdir}/collectd-$PKGVER"
+
+	./configure --host=$CROSS_COMPILE \
+		--disable-werror \
+		--prefix=/opt \
+		--runstatedir=/var/run \
+		--with-fp-layout=nothing --disable-pcie_errors
+
+	rm -rf installroot
+	mkdir  installroot
+	make install DESTDIR=$PWD/installroot
+}
+
+package() {
+	cd "${srcdir}/collectd-$PKGVER"
+
+	# FIXME get rid of the .la files
+	find installroot -iname '*.la' -exec rm {} \;
+
+	# get rid of other files
+	find installroot -type f -path '*/perl/*'      -exec rm -f {} \;
+	find installroot -type f -path '*/pkgconfig/*' -exec rm -f {} \;
+	find installroot -not -type d -iname 'libcollectdclient*' -exec rm -f {} \;
+	find installroot -type f -path '*/bin/collectd*' -exec rm -f {} \;
+	find installroot -type f -path '*/sbin/collectdmon' -exec rm -f {} \;
+	find installroot -depth -empty -exec rmdir {} \;
+
+	# add in our files
+	install -m0644 -D $srcdir/files/collectd.control installroot/opt/lib/ipkg/info/collectd.control
+	install -m0755 -D $srcdir/files/S99collectd      installroot/opt/etc/init.d/S99collectd
+
+	# package files
+	rm -f "$builddir/$OUT_TARBALL"
+	fakeroot -- tar czvf "$builddir/$OUT_TARBALL" -C installroot/opt sbin lib etc share/collectd
+}
+
+srcdir=$PWD
+[ ! -d "$srcdir" ] && mkdir "$srcdir"
+cd "$srcdir"
+
+# make build dir
+builddir="$srcdir/build"
+[ -d "$srcdir/build" ] || mkdir "$srcdir/build"
+
+export LDFLAGS="-s"
+
+if [ "$#" -lt 1 ]; then
+	extract
+	prepare
+	build
+	package
+else
+	type $1 >/dev/null && $1 || echo "invalid build stage $1"
+fi
 
